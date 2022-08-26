e983f229e536fcfeb84d589cbf556a8a7d6ddcc9
Mosharaf456 <mosharaf111hossain@gmail.com>
2022-08-27 02:32:27 +0600

    React JS Counter APP is Created by Myself

---
+++ src/components/app.component.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import Box from "./box.component";
+
+class App extends Component {
+  state = {
+    boxes: []
+  };
+  
+  createBox = () => {
+    const { boxes } = this.state;
+    this.setState( {boxes: [...boxes, 0] } );
+  }
+  
+  onDelete = (id) => {
+    function filterArray (number, index) {
+      if(id === index) return false;
+      else return true;
+    }
+    const updatedBoxes = this.state.boxes.filter(filterArray);
+
+    this.setState({ boxes: updatedBoxes });
+
+  }
+
+  onIncrement = (id) => {
+    const arr = [...this.state.boxes];
+    arr[id]++;
+    this.setState({ boxes: arr});
+  }
+
+  onDecrement = (id) => {
+    const arr = [...this.state.boxes];
+    if (arr[id] != 0) {
+      arr[id]--;
+    }
+    this.setState({ boxes: arr});
+  }
+
+  render() {
+    return (
+      <div style={{ margin: "15px" }}>
+        <button className='btn btn-primary p-2' onClick = { this.createBox } >Add New Box</button>
+        <br />
+        <br />
+        {this.state.boxes.map((number, index) => (
+          <div key={index}>
+            <Box  
+              num = { number } 
+              id = {index} 
+              onDelete = {this.onDelete} 
+              onIncrement = { this.onIncrement} 
+              onDecrement = { this.onDecrement }
+            />
+            <br />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app.component';
+
+const div = document.getElementById('root');
+const root = ReactDOM.createRoot(div);
+root.render(
+    <App />
+);
+
+
